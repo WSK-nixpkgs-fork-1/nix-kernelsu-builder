28b4efc2bb5f667ed0aec061b7402bd41c1eb29f
wsk2007 <wangshengkai2007@gmail.com>
2024-07-15 13:23:19 +0800

    Add oneplus 7t kernel

--- kernels.nix
+++ kernels.nix
@@ -3,32 +3,7 @@ let
   pipeline = callPackage ./pipeline { };
 in
 {
-  amazon-fire-hd-karnak = pipeline {
-    anyKernelVariant = "osm0sis";
-    enableKernelSU = false;
-    kernelDefconfigs = [ "lineageos_karnak_defconfig" ];
-    kernelImageName = "Image.gz-dtb";
-    kernelMakeFlags = [
-      "KCFLAGS=\"-w\""
-      "KCPPFLAGS=\"-w\""
-    ];
-    kernelSrc = sources.linux-amazon-karnak.src;
-    oemBootImg = boot/amazon-fire-hd-karnak.img;
-  };
 
-  moto-rtwo-lineageos-21 = pipeline {
-    anyKernelVariant = "kernelsu";
-    clangVersion = "latest";
-    kernelDefconfigs = [
-      "gki_defconfig"
-      "vendor/kalama_GKI.config"
-      "vendor/ext_config/moto-kalama.config"
-      "vendor/ext_config/moto-kalama-gki.config"
-      "vendor/ext_config/moto-kalama-rtwo.config"
-    ];
-    kernelImageName = "Image";
-    kernelSrc = sources.linux-moto-rtwo-lineageos-21.src;
-  };
 
   oneplus-8t-blu-spark = pipeline {
     anyKernelVariant = "osm0sis";
